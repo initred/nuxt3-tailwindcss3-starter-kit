10bfb63818d5a344ebcd22831f4aad77abc7f372
initred <initred@gmail.com>
2023-02-08 23:50:51 +0900

    fix: tailwind.config.js add plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,11 @@ module.exports = {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries')
+  ]
 }
