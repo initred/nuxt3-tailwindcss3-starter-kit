fc2071fac51e202b087b1e7eadfed4096b21cd32
initred <initred@gmail.com>
2023-04-01 18:15:40 +0900

    chore: deps update

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/container-queries')
   ]
