072fd6d5c753a8acd243e387f234d4dcad3ef8a2
initred <initred@gmail.com>
2023-08-30 21:00:07 +0900

    fixed: tailwind.config.js => tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: [
     './components/**/*.{js,vue,ts}',
     './composables/**/*.{js,vue,ts}',
@@ -20,4 +21,4 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/container-queries')
   ]
-}
+} satisfies Config
